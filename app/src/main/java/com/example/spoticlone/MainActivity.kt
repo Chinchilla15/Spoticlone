52c9e60a43ebc61333814205120f25361fef4796
Alessandro Chinchilla <alessandrochinchilla@ravn.co>
2025-03-27 13:51:35 -0600

    feat: login page with validation logic

--- app/src/main/java/com/example/spoticlone/MainActivity.kt
+++ app/src/main/java/com/example/spoticlone/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.example.spoticlone
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_login)
+
+        // Login logic
+        val usernameInput = findViewById<EditText>(R.id.usernameEditText)
+        val passwordInput = findViewById<EditText>(R.id.passwordEditText)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        val forgotPasswordText = findViewById<TextView>(R.id.forgotPasswordText)
+        val signupText = findViewById<TextView>(R.id.signupText)
+
+        loginButton.setOnClickListener {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+
+            if (username == "user" && password == "password") {
+                Toast.makeText(this, "¡Inicio de sesión exitoso!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Credenciales inválidas", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        forgotPasswordText.setOnClickListener {
+            Toast.makeText(this, "Clic en Olvidar contraseña", Toast.LENGTH_SHORT).show()
+        }
+
+        signupText.setOnClickListener {
+            Toast.makeText(this, "Clic en Registrarse", Toast.LENGTH_SHORT).show()
         }
     }
 }
